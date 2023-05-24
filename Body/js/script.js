c1cbf31cba20fb440fb5137eb705be17ea6bacae
Brayan MG <107977576+Brayan-CG@users.noreply.github.com>
2023-05-23 19:34:31 -0500

    Add files via upload

--- Body/js/script.js
+++ Body/js/script.js
@@ -1,3 +1,33 @@
-function abrir(){
-   document.getElementById("Router").style.display="block";
+function inicio(){
+   location.href="go:1"
+}
+function app(){
+   document.getElementById("app").style.display="block"
+}
+function github(){
+   document.getElementById("github").style.display="block"
+}
+function admob(){
+   document.getElementById("admob").style.display="block"
+}
+function ads(){
+   document.getElementById("ads").style.display="block"
+}
+function aternos(){
+   document.getElementById("aternos").style.display="block"
+}
+function firebase(){
+   document.getElementById("firebase").style.display="block"
+}
+function netlify(){
+   document.getElementById("netlify").style.display="block"
+}
+function volver(){
+   document.getElementById("app").style.display="none";
+   document.getElementById("github").style.display="none";
+   document.getElementById("admob").style.display="none";
+   document.getElementById("ads").style.display="none";
+   document.getElementById("aternos").style.display="none";
+   document.getElementById("firebase").style.display="none";
+   document.getElementById("netlify").style.display="none";
 }
